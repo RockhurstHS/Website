2038664b8e2b5d8e1c0d47f2e213c601d091455c
JamesWClark <jwclark@rockhursths.edu>
2015-01-11 00:54:40 -0600

    add js hack to force blue event bar out of its container

---
+++ scripts/various-hacks.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	positionEventHighlights();
+});
+
+function positionEventHighlights() {
+	var sectionC = $('#home #wrapSectionSecondary .sectionC:hidden');
+	$(sectionC).appendTo('#home-alert-box');
+	$(sectionC).show();
+	/** css doesn't refresh all the way */
+	$('#home-alert-box .sectionC .gutter h4').css('color', 'white');
+}
