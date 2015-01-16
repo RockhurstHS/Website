e4a81d686e6b93f427ffb4eefc6cdc0331330456
jwclark <jwclark@rockhursths.edu>
2015-01-16 15:58:54 -0600

    beginning work on accordion nav

--- scripts/various-hacks.js
+++ scripts/various-hacks.js
@@ -1,9 +1,11 @@
-﻿$(document).ready(function () {
-	positionEventHighlights();
-	$(window).on('resize', positionEventHighlights);
+﻿
+$(document).ready(function () {
+	respond();
+	$(window).on('resize', respond);
 });
 
-function positionEventHighlights() {
+function respond() {
+	//position blue event highlights
 	if (window.innerWidth < 631) {
 		var sectionC = $('#home #wrapSectionSecondary .sectionC:hidden');
 		$(sectionC).appendTo('#home #home-alert-box');
@@ -11,5 +13,15 @@ function positionEventHighlights() {
 	} else {
 		var sectionC = $('#home #home-alert-box .sectionC');
 		$(sectionC).insertAfter('#home #wrapSectionSecondary .sectionB');
-	}	
+	}
+	//blue navigation as accordion
+	if (window.innerWidth < 481) {
+		$("#wrapNav .menu").accordion({
+			collapsible: true,
+			active: false,
+			heightStyle: "content"
+		});
+	} else {
+		$("#wrapNav .menu").accordion("destroy");
+	}
 }
