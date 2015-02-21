d543f9effe619bc97dddbd8ca36a38e3c351a7d8
JamesWClark <jwclark@rockhursths.edu>
2015-02-21 01:55:47 -0600

    finish 480px deans page demo 1

--- scripts/various-hacks.js
+++ scripts/various-hacks.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
 	respond();
 	$(window).on('resize', respond);
+	notifyBrowserUpdate();
 });
 
 function respond() {
@@ -25,4 +26,15 @@ function respond() {
 		$("#wrapNav .menu").accordion("destroy");
 		$('#wrapNav .menu li a').removeClass('ui-state-focus');
 	}
+}
+/* this won't work, gotta move out this inner function i think*/
+function notifyBrowserUpdate() {
+	var $buoop = { c: 2 };
+	function $buo_f() {
+		var e = document.createElement("script");
+		e.src = "//browser-update.org/update.js";
+		document.body.appendChild(e);
+	};
+	try { document.addEventListener("DOMContentLoaded", $buo_f, false) }
+	catch (e) { window.attachEvent("onload", $buo_f) }
 }
